51ad1538df1e1b4fdbd16c0ee28a995c43defdf4
Nigel Chen <nigelchen2014@gmail.com>
2014-11-17 12:27:19 -0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,19 @@
+import java.awt.Desktop;
+import java.net.URI;
+import java.lang.Object;
+import javax.swing.JOptionPane;
+class BrowseURL
+{
+  public static void main(String args[]) throws Exception
+ {
+  String foo = JOptionPane.showInputDialog(null,"Enter a website you want to go to!", "Enter URL here!");
+ Desktop d=Desktop.getDesktop();
+
+ if (foo.matches(".*http://.*")) {
+    d.browse(new URI(foo));
+ }
+ else {
+ d.browse(new URI("http://" + foo));
+ }
+ }
+}
